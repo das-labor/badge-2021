7bc76d7e097d37b74c943f7bb802a7fab67cee68
Marco Bakera <marco@bakera.de>
2021-04-08 22:56:12 +0200

    fix demo: using monochrome

--- software/demo.py
+++ software/demo.py
@@ -8,27 +8,27 @@ def handle_px(t, x, y):
     # -1 < sin() < +1
     # therefore correct the value and bring into range [0, 1]
     v = (v+1.0) / 2.0
-    return v
+    if v>0.5: return 1
+    else: return 0
 
 i = 0
 oled = SSD1306_I2C(addr=0x78)
 
 while True:
     t = time.time()
-    for y in range(oled.height):
+    oled.fill_rect(0,0, oled.width,oled.height,0)
+    for y in range(oled.height//2,oled.height):
         for x in range(oled.width):
-            v = int(handle_px(t, x, y) * 120)
-            oled.pixel(x, y, (v,v,0))
+            v = handle_px(t, x, y)
+            oled.pixel(x, y, v)
 
-    oled.hline(0, oled.height*(math.sin(3*t)+1)//2, oled.width,
-        (100,0,200))
+    oled.hline(0, oled.height//2*(math.sin(3*t)+1)//2, oled.width, 1)
 
-    oled.vline(2*i,0,oled.height, (100,0,200))
-    oled.text("Das Labor", i, 10, (100,200,200))
-    oled.text("Badge 2021", i+20, 30, (0,200,100))
+    oled.vline(2*i,0,oled.height//2, 1)
+    oled.text("Das Labor", i, 0, 1)
+    oled.text("Badge 2021", i+20, 10, 1)
 
-    oled.rect(i+18, 30, 65, 10, (200,200,200))
-    #oled.fill_rect(i+18, 45, 65, 5, (200,200,200))
+    oled.rect(i+18, 10, 65, 10, 1)
     #oled.blit(oled, 10,10)
     #oled.scroll(i,i)
     i = -80 if i+1>oled.width else i+1
